41e0c9681264468125ff5cf371762a3ae897f054
Azat Gabdrakhmanov <azat@MacBook-Air-Azat.local>
2023-04-24 18:14:50 +0300

    Fix test

--- UsingFiniteField3/Program.cs
+++ UsingFiniteField3/Program.cs
@@ -1,6 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System.Text;
 using UsingFiniteField3;
 
-Console.WriteLine("Hello, World!");
+byte[] Polynom = new byte[] { 104, 169, 157, 91 };
+var _crc1 = new Controllsum(Polynom);
+
+var actualCheckSum = _crc1.GetSumm(new byte[] { 104, 169, 157, 91 });
+var _crc1q = new Controllsum(Polynom);
+
+
